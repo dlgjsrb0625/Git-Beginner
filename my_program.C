6fe8fad4a77fca021e50c8e32b51727b3f94fb07
dlgjrsb0625 <gjdrnflrnfl0@naver.com>
2019-03-19 23:30:24 +0900

    Add my_program [Sub]

--- my_program.C
+++ my_program.C
@@ -1,15 +1,32 @@
 #include<stdio.h>
 
+int add(int, int);
+int sub(int, int);
+
 main()
 {
-
-
-
-	printf("Good!!");
-
-	return 0;
-
-
-
+	int a=5, b=3, y;
+    y = add(a,b);
+	y = y-8;
+	printf("SUM 2= %d\n", y);
+	y = sub(a,b);
+	printf("SUM 22 = %d\n", y);
 
 }
+
+int add(int c, int d)
+{
+	int sum;
+	sum = c + d;
+	return sum;
+	printf("SUM = %d\n", sum);
+}
+
+int sub(int c, int d)
+{
+	int sum;
+	sum = c - d;
+	return sum;
+	printf("SUM = %d\n", sum);
+}
+
